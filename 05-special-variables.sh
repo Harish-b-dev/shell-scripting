8e7f7b6934a511670633deedf10b8a206c702ee1
harish boppana <boppanaharish7475@gmail.com>
2026-02-12 16:34:33 +0530

    actions

---
+++ 05-special-variables.sh
@@ -0,0 +1,25 @@
+#!/bin/shell
+
+person1=$1
+person2=$2
+
+echo "Hi $person1, how are you?"
+echo "Hi $person2, I'good how are you?"
+
+echo "Hi I'm fine $person1 what are are your palns for weekend."
+echo "$person2 :: going to movie, what about you."
+echo "$person1 :: I need to practise shell scripting."
+
+
+
+#special variables
+echo "Variables passed in command $@"
+echo "No of variables passed in command $#"
+echo "Script name $0"
+echo "Present working directory $pwd"
+echo "Who is running the sctript $USER"
+echo "Home directory of the user $HOME"
+echo "PID of the script $$"
+echo "Background process of the script $!"
+echo "Variables passed in command $*"
+echo "exit status of the previous command $?"
