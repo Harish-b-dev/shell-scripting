26877011b2f7ced9e22c3513e7c6f838ef5f160a
harish boppana <boppanaharish7475@gmail.com>
2026-02-12 15:06:57 +0530

    actions

--- 01-variables.sh
+++ 01-variables.sh
@@ -5,3 +5,7 @@ person2=Suresh
 
 echo "Hi $person1, how are you?"
 echo "Hi $person2, I'good how are you"
+
+echo "Hi I'm fine $person1 what are are your palns for weekend"
+echo "$person2 :: going to movie, what about you"
+echo "$person1 :: I need to practise shell scripting"
