9d0e1b214623b54054e184df79ae1e8a21894701
harish boppana <boppanaharish7475@gmail.com>
2026-02-12 17:11:52 +0530

    actions

---
+++ 07-conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/shell
+
+number=25
+
+# -gt or > -- for greater than
+# -lt or < -- for less than
+# -eq or == -- for equalto
+# -ne or != -- for not equalto
+# -ge or >= -- for greater than or equal to
+# -le or <= -- for less than or equal to
+
+if [ $number -ge $1 ]; then
+    echo "Your number is greater than the required number"
+
+else
+    echo "Your number is less than the required number"
