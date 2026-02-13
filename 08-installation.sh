be59cad8bb4531a1b28050e75366deb9a5d4d94c
harish boppana <boppanaharish7475@gmail.com>
2026-02-13 15:41:08 +0530

    actions

---
+++ 08-installation.sh
@@ -0,0 +1,16 @@
+#!/bin/shell
+
+user_code=$(id -u)
+
+if [ $user_code -ne 0 ]; then
+    echo "You need sudo access to install nginx."
+else
+    echo "Installing nginx..."
+    dnf install nginx
+
+    #if [ $? -eq 0 ]; then
+    #    echo "Installing nginx... Success"
+    
+    #else
+    #    echo "Installing nginx... Failure"
+fi
