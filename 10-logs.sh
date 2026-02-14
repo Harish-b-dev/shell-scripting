f9d67d028f3b7f471b1d6f6468db68a8af80cba9
harish boppana <boppanaharish7475@gmail.com>
2026-02-14 18:25:18 +0530

    actions

--- 10-logs.sh
+++ 10-logs.sh
@@ -7,7 +7,7 @@ log_file="/var/log/shell-practise/$0.log"
 mkdir -p $logs_folder
 
 if [ $user -ne 0 ]; then 
-    echo "You need sudo access to install packages." | tee $log_file
+    echo "You need sudo access to install packages." | tee -a $log_file
     exit 1
 
 fi
@@ -17,11 +17,11 @@ install_fun(){
     dnf install $1 -y &>> $log_file
 
     if [ $? -ne 0 ]; then
-        echo "$1  installation ... failure" | tee $log_file
+        echo "$1  installation ... failure" | tee -a $log_file
         exit 1
 
     else
-        echo "$1 installation ... success" | tee $log_file
+        echo "$1 installation ... success" | tee -a $log_file
     fi
 }
 
