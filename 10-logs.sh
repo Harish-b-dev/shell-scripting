42a452dee6215093273128ed41e661fc1725653b
harish boppana <boppanaharish7475@gmail.com>
2026-02-14 18:12:00 +0530

    actions

---
+++ 10-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/shell
+
+user=$(id -u)
+logs_folder="/var/log/shell-practise"
+log_file="/var/log/shell-practise/$0.log"
+
+mkdir -p $logs_folder
+
+if [ $user -ne 0 ]; then 
+    echo "You need sudo access to install packages." &>> $log_file
+    exit 1
+
+fi
+
+
+install_fun(){
+    dnf install $1 -y &>> $log_file
+
+    if [ $? -ne 0 ]; then
+        echo "$1  installation ... failure" &>> $log_file
+        exit 1
+
+    else
+        echo "$1 installation ... success" &>> $log_file
+    fi
+}
+
+install_fun nginx
+
+install_fun mysql
+
+install_fun nodejs
