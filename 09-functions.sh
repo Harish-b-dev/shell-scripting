1816c8105c751dfd78416201a93947c0c82d5968
harish boppana <boppanaharish7475@gmail.com>
2026-02-14 17:13:43 +0530

    actions

---
+++ 09-functions.sh
@@ -0,0 +1,38 @@
+#!/bin/shell
+
+user=$(id -u)
+
+if [ $user -ne 0 ]; then 
+    echo "You need sudo access to install packages."
+    exit 1
+
+fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]:
+    echo "nginx installlation ... failure"
+
+else
+    echo "nginx installation ... success"
+fi
+
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]:
+    echo "nginx installlation ... failure"
+
+else
+    echo "nginx installation ... success"
+fi
+
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]:
+    echo "nginx installlation ... failure"
+
+else
+    echo "nginx installation ... success"
+fi
