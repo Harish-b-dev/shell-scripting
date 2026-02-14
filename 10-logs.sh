f671fb8ab4bebe5669794935bac359a6b705d6c1
harish boppana <boppanaharish7475@gmail.com>
2026-02-14 18:41:55 +0530

    actions

--- 10-logs.sh
+++ 10-logs.sh
@@ -5,6 +5,8 @@ logs_folder="/var/log/shell-practise"
 log_file="/var/log/shell-practise/$0.log"
 command_type=$1
 
+echo "command passed ... $command_type"
+
 mkdir -p $logs_folder
 
 if [ $command_type -eq "install" ]; then
@@ -45,14 +47,14 @@ elif [ $command_type -eq "remove" ]; then
 
 
     install_fun(){
-        dnf install $2 -y &>> $log_file
+        dnf install $1 -y &>> $log_file
 
         if [ $? -ne 0 ]; then
-            echo "$2  installation ... failure" | tee -a $log_file
+            echo "$1  installation ... failure" | tee -a $log_file
             exit 1
 
         else
-            echo "$2 installation ... success" | tee -a $log_file
+            echo "$1 installation ... success" | tee -a $log_file
         fi
     }
 
