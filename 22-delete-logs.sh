f88a10d78bdf46ad428756475239a7c471b8c58b
harish boppana <boppanaharish7475@gmail.com>
2026-02-21 21:54:42 +0530

    actions

--- 22-delete-logs.sh
+++ 22-delete-logs.sh
@@ -6,11 +6,12 @@ B="\e[33m"
 Y="\e[34m"
 
 folder="/home/ec2-user"
-logs_folder="$folder/$0.log"
+logs_folder="$folder/deletions"
+log_file="$folder/$0.log"
 
-sudo touch -p $logs_folder
+sudo mkdir -p $logs_folder
 
-checking_folder="$folder/app/"
+checking_folder="$folder/app"
 
 if [ ! -d $checking_folder ]; then
     echo -e "$Y Folder does not exist.$N"
@@ -22,8 +23,8 @@ logs_to_delete=find ./ -type f -mtime +14
 
 while IFS= read -r file;
 do
-    echo -e "deleting file$Y $file $N" | tee -a $logs_folder
-    #rm $file &>> $logs_folder
-    echo -e "file $R $file $N deletion successful." | tee -a $logs_folder
+    echo -e "deleting file$Y $file $N" | tee -a $log_file
+    rm $file &>> $log_file
+    echo -e "file $R $file $N deletion successful." | tee -a $log_file
 
 done <<< $logs_to_delete
