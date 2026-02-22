0b9ece74dc082cc4c4e44b1267e1f50cd39b2614
harish boppana <boppanaharish7475@gmail.com>
2026-02-22 19:03:32 +0530

    actions

---
+++ 23-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+folder="/var/log/shell-script"
+logs_folder="$folder/backup.log"
+
+sudo mkdir -p $folder
+
+log(){
+    echo -e "$(date "%Y-%m-%d -- %H:%M:%S") | $1" | tee -a $logs_folder
+}
+
+log printing successful
+
+
