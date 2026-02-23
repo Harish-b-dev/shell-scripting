0cb1293252510055c9aa2a31084bc3e4a4b24593
harish boppana <boppanaharish7475@gmail.com>
2026-02-23 12:06:55 +0530

    actions

--- 24-disk-usage.sh
+++ 24-disk-usage.sh
@@ -19,10 +19,10 @@ do
     disk_usage=$($line | awk -F ' ' '{print $6}' |cut -d '%' -f1 | grep -vi use)
     disk_name=$($line | awk -F ' ' '{print $1}' |cut -d '%' -f1 | grep -vi filesystem)
 
-    if [ $disk_usage -ge $alert_usage1 && $line -lt $alert_usage2 ]; then
+    if [ "$disk_usage" >= "$alert_usage1" && "$disk_usage" < "$alert_usage2" ]; then
         log "$Y Disk storage utilization is${N} ${B}$disk_usage${N} ${Y}for$N ${$G}$disk_name.$N"
     
-    elif [ $disk_usage -ge $alert_usage2 ]; then
+    elif [ "$disk_usage" >= "$alert_usage2" ]; then
         log "$R Disk storage utilization is${N} ${B}$disk_usage${N} ${R}for$N ${$Y}$disk_name.$N"
     fi
 done <<< $disk
