83f50df8d7ded1a991a0a05f959f2987c99a3fc1
harish boppana <boppanaharish7475@gmail.com>
2026-02-24 15:04:08 +0530

    actions

--- mail.sh
+++ mail.sh
@@ -9,10 +9,10 @@ Alert_type2=$6
 #body=$7
 
 
-message_body1=$(echo $Alert_type1 | sed -e 's:/:\\:g')
-message_body2=$(echo $Alert_type2 | sed -e 's:/:\\:g')
+message_body1=$(echo $Alert_type1)
+message_body2=$(echo $Alert_type2)
 
-mail_body=$(sed -e "s/To_Team/$To_Team/g" -e "s/Public_IP/$IP/g" -e "s/MESSAGE1/$message_body1/g" -e "s/MESSAGE2/$message_body2/g" template.html)
+mail_body=$(sed -e "s/To_Team/$To_Team/g" -e "s/Public_IP/$IP/g" -e "s|MESSAGE1|$message_body1|g" -e "s|MESSAGE2|$message_body2|g" template.html)
 
 {
 echo "To: $To_Mail"
